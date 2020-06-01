e6dc0be5c83ec991e3b0e9d503e8d848eaf6b903
Alexandros Collie <amcollie@gmail.com>
2020-06-01 03:16:45 -0400

    Add user session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    byebug
+    user_session = params[:session]
+    user = User.find_by(email: user_session[:email].downcase)
+    if user&.authenticate(user_session[:password])
+      session[:user_id] = user_id
+      redirect_to user, notice:  'Logged in successfully'
+    else
+      flash.now[:alert] = 'There was something wrong with log in details'
+      render 'new'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: 'Logged out'
   end
 end
